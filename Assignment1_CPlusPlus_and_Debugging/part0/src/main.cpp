ec029fbfb76c61568b7dbca6f2b7078ed1e8aabe
dark7torm <rxtren2022@gmail.com>
2023-05-12 22:22:24 -0400

    Part 0 Complete

--- Assignment1_CPlusPlus_and_Debugging/part0/src/main.cpp
+++ Assignment1_CPlusPlus_and_Debugging/part0/src/main.cpp
@@ -4,9 +4,37 @@
 #include <iostream> // input and output 'stream' library
 #include <vector>   // std::vector data structure.
 
+void PrintVector(const std::vector<int>& v) {
+	std::cout << "PrintVector" << std::endl;
+	for(int i = 0; i < v.size(); i++) {
+		std::cout << v.at(i) << std::endl;
+	}
+}
+
+void PrintRawArray(int array[], int size) {
+	std::cout << "PrintRawArray" << std::endl;
+	for(int i = 0; i < size; i++) {
+		std::cout << array[i] << std::endl;
+	}
+}
+
 int main(){
-	
-	// Source code here
-	
+	std::cout << "Hello my name is Ren" << std::endl;
+
+	// Raw array
+	int array[3] = {1, 2, 3};
+
+
+	// this is a data structure that abstracts around a raw array, 
+	// so it is a little safer, and can have some member functions
+	// do some actions
+	std::vector<int> myVector;
+	myVector.push_back(1);
+	myVector.push_back(2);
+	myVector.push_back(3);
+	PrintVector(myVector);
+	PrintRawArray(myVector.data(), myVector.size());
 	return 0;
+
+
 }
